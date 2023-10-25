3d5a67d9c73de47310cb18e426515790b37d8d9a
madr89 <mat.drozdzewski@gmail.com>
2023-10-25 09:31:58 +0200

    Add files via upload
    script to concatenate tsf files into one file

---
+++ concatenate.ps1
@@ -0,0 +1,11 @@
+﻿$dir = "C:\Users\mateusz.drozdzewski\Documents\EPOS.PL_plus\kampaniaEpos\gPhoneX"
+# Build the file list
+$fileList = Get-ChildItem -Path $dir -Filter *.tsf -File
+$outFile = Join-Path $dir "merged.tsf"
+# Get the header info from the first file
+Get-Content $fileList[0] | select -First 90 | Out-File -FilePath $outfile -Encoding ascii
+# Cycle through and get the data (sans header) from all the files in the list
+foreach ($file in $filelist)
+{
+    Get-Content $file | select -Skip 90 | Out-File -FilePath $outfile -Encoding ascii -Append
+}
